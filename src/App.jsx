0f455492b0a95853b4bcbf6036e7e34d214fd0c6
Margo Giddens <giddens.margo@gmail.com>
2024-08-19 14:23:54 -0500

    map over puppies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import { puppyList } from './data.js'
+import { useState } from 'react';
 
-function App() {
-  
-
+function App() { 
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <>
       <div>
-        
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })}
       </div>
     </>
   )
