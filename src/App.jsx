5ba09b115e6a6ab673f7825f98dca6a2858cfc7f
Margo Giddens <giddens.margo@gmail.com>
2024-08-19 16:07:50 -0500

    featured pup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import './App.css'
-import { puppyList } from './data.js'
-import { useState } from 'react';
+import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from "react";
 
-function App() { 
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log("featured pup: ", featuredPup);
+
   return (
     <>
       <div>
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
         })}
+        {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
